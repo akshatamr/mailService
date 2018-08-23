a88d0f0d5a4bae7839fb8007391c14d408bf9af6
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 22:58:13 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 'use strict';
 const nodemailer = require('nodemailer');
 
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
-//nodemailer.createTestAccount((err, account) => {
+nodemailer.createTestAccount((err, account) => {
     // create reusable transporter object using the default SMTP transport
-  //  let transporter = nodemailer.createTransport({
-    //    host: 'smtp.ethereal.email',
-      //  port: 587,
-        //secure: false, // true for 465, false for other ports
-        //auth: {
-          //  user: account.user, // generated ethereal user
-           // pass: account.pass // generated ethereal password
-       // }
-   // });
-
-    // setup email data with unicode symbols
+    let transporter = nodemailer.createTransport({
+       host: 'smtp.ethereal.email',
+       port: 587,
+       secure: false, // true for 465, false for other ports
+       auth: {
+            user: 'automataaug2018@gmail.com', // generated ethereal user
+            pass: 'Auto@2018' // generated ethereal password
+        }
+    });
+    
+   // setup email data with unicode symbols
     let mailOptions = {
         from: 'automataaug2018@gmail.com', // sender address
         to: 'autodroid2018@gmail.com', // list of receivers
