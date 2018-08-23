88bd1bd79dff9be7eb374c0e729f13790ae35a90
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 23:13:05 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-'use strict';
-const nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer');
 
-nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-       host: 'smtp.ethereal.email',
-       port: 465,
-       secure: true, // true for 465, false for other ports
-       auth: {
-            user: 'automataaug2018@gmail.com', // generated ethereal user
-            pass: 'Auto@2018' // generated ethereal password
-        }
-    });
-    
-   // setup email data with unicode symbols
-    let mailOptions = {
-        from: 'automataaug2018@gmail.com', // sender address
-        to: 'autodroid2018@gmail.com', // list of receivers
-        subject: 'Hello from eGreen', // Subject line
-        text: 'Hello people from eGreen', // plain text body
-        html: '<b>Hello people</b>' // html body
-    };
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'automataaug2018@gmail.com',
+    pass: 'Auto@2018'
+  }
+});
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    });
+var mailOptions = {
+  from: 'automataaug2018@gmail.com',
+  to: 'autodroid2018@gmail.com',
+  subject: 'Sending Email for eGreen',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
 });
