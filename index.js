98213ae849815e33e062b740e6c8754da7a0c1eb
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 16:06:00 +1000

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
  
-var action = req.body.queryResult.action;
 var mailer   = require("mailer")
   , username = "apikey"
   , password = "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";
@@ -31,21 +30,17 @@ var mailer   = require("mailer")
   , username:       username
   , password:       password
   });
+
   
   return res.json({
-    //fulfillmentText: chatResponse
-    fulfillmentText: "test"
-    /*text: x
-    departingCity: departCity,
-    arrivalCity: arrivalCity,
-    startDate: startDate,
-    endDate: endDate*/
+   fulfillmentText: "eGreen Test Success"
    
   });
-  
-});  
 });
 
+function myData() { 
+   chatResponse=chatResponse+"hello";
+} 
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
