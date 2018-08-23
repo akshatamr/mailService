42eb71bb58e5b3ce7ff0c6ad806f26db82b4436a
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 23:06:53 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ nodemailer.createTestAccount((err, account) => {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
        host: 'smtp.ethereal.email',
-       port: 587,
-       secure: false, // true for 465, false for other ports
+       port: 465,
+       secure: true, // true for 465, false for other ports
        auth: {
             user: 'automataaug2018@gmail.com', // generated ethereal user
             pass: 'Auto@2018' // generated ethereal password
@@ -30,8 +30,5 @@ nodemailer.createTestAccount((err, account) => {
         console.log('Message sent: %s', info.messageId);
         // Preview only available when sending through an Ethereal account
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     });
 });
