18f8a694d141e36393d23745012f6c40d3a940b7
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 11:50:01 +1000

    Create mailer.js

---
+++ mailer.js
@@ -0,0 +1,20 @@
+var mailer   = require("mailer")
+  , username = "automataaug2018@gmail.com"
+  , password = "Auto@2018";
+
+mailer.send(
+  { host:           "smtp.mandrillapp.com"
+  , port:           25
+  , to:             "autodroid2018@gmail.com"
+  , from:           "automataaug2018@gmail.com"
+  , subject:        "Green Test"
+  , body:           "Hello from eGreen!"
+  , authentication: "login"
+  , username:       username
+  , password:       password
+  }, function(err, result){
+    if(err){
+      console.log(err);
+    }
+  }
+);
