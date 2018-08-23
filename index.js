2e24b3efbba61037c19557617a5e29e2981552da
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 14:58:32 +1000

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,23 @@ jsonFile.readFile(fileName, function(err, jsonData) {
   , username = "automataaug2018@gmail.com"
   , password = "Auto@2018";
   
+  mailer.send(
+  { host:           "smtp.mandrillapp.com"
+  , port:           8000
+  , to:             "autodroid2018@gmail.com"
+  , from:           "automataaug2018@gmail.com"
+  , subject:        "Green Test"
+  , body:           "Hello from eGreen!"
+  , authentication: "login"
+  , username:       username
+  , password:       password
+  }, function(err, result){
+    if(err){
+      console.log(err);
+    }
+  }
+);
+  
   return res.json({
     //fulfillmentText: chatResponse
     fulfillmentText: "test"
