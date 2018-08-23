ed114eef0c0ea61aba25f9c3edd1e7c655305044
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 14:50:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -31,42 +31,6 @@ jsonFile.readFile(fileName, function(err, jsonData) {
 });
  
   
-  
-  
-  var departCity =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.departCity
-      ? req.body.queryResult.parameters.departCity
-      : "Test Data";
-  
-  var arrivalCity =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.arrivalCity
-      ? req.body.queryResult.parameters.arrivalCity
-      : "Test Data";
-  
-    var startDate =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.startDate
-      ? req.body.queryResult.parameters.startDate
-      : "Test Data";
-  
-    var endDate =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.endDate
-      ? req.body.queryResult.parameters.endDate
-      : "Test Data";
-  
-  //var x = "";
-  /*if (action == 'book_tickets')
-  {
-    x = "Action was successful.";
-  } */
-  
   return res.json({
     //fulfillmentText: chatResponse
     fulfillmentText: "test"
