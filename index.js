a65349a251f218f729f3f390473bc5537c8939e2
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 22:35:56 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,105 +1,39 @@
-"use strict";
+'use strict';
+const nodemailer = require('nodemailer');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+//nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+  //  let transporter = nodemailer.createTransport({
+    //    host: 'smtp.ethereal.email',
+      //  port: 587,
+        //secure: false, // true for 465, false for other ports
+        //auth: {
+          //  user: account.user, // generated ethereal user
+           // pass: account.pass // generated ethereal password
+       // }
+   // });
 
-const restService = express();return res.json({
-    //fulfillmentText: chatResponse
-    fulfillmentText: chatResponse
-    /*text: x
-    departingCity: departCity,
-    arrivalCity: arrivalCity,
-    startDate: startDate,
-    endDate: endDate*/
-   
-  });
-});
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: 'automataaug2018@gmail.com', // sender address
+        to: 'autodroid2018@gmail.com', // list of receivers
+        subject: 'Hello from eGreen', // Subject line
+        text: 'Hello people from eGreen', // plain text body
+        html: '<b>Hello people</b>' // html body
+    };
 
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-restService.use(bodyParser.json());return res.json({
-    //fulfillmentText: chatResponse
-    fulfillmentText: chatResponse
-    /*text: x
-    departingCity: departCity,
-    arrivalCity: arrivalCity,
-    startDate: startDate,
-    endDate: endDate*/
-   
-  });
-});
-
-restService.post("/echo", return res.json({
-    //fulfillmentText: chatResponse
-    fulfillmentText: chatResponse
-    /*text: x
-    departingCity: departCity,
-    arrivalCity: arrivalCity,
-    startDate: startDate,
-    endDate: endDate*/
-   
-  });
-});(req, res) {return res.json({
-    //fulfillmentText: chatResponse
-    fulfillmentText: chatResponse
-    /*text: x
-    departingCity: departCity,
-    arrivalCity: arrivalCity,
-    startDate: startDate,
-    endDate: endDate*/
-   
-  });
-});
-
-var nodemailer = require('nodemailer');
-return res.json({
-    //fulfillmentText: chatResponse
-    fulfillmentText: chatResponse
-    /*text: x
-    departingCity: departCity,
-    arrivalCity: arrivalCity,
-    startDate: startDate,
-    endDate: endDate*/
-   
-  });
-});
-var transporter = nodemailer.return res.json({
-    //fulfillmentText: chatResponse
-    fulfillmentText: chatResponse
-    /*text: x
-    departingCity: departCity,
-    arrivalCity: arrivalCity,
-    startDate: startDate,
-    endDate: endDate*/
-   
-  });
-});({
-  service: 'gmail',
-  auth: {
-    user: 'automataaug2018@gmail.com',
-    pass: 'Auto@2018'
-  }
-});
-
-var mailOptions = {
-  from: 'automataaug2018@gmail.com',
-  to: 'autodroid2018@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-return res.json({
-   fulfillmentText: "test"   
-  });
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
 });
