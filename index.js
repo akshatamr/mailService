10c88fadea471d8e233a2254581f75c3ebe55aea
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-23 17:08:11 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,26 @@
 "use strict";
 
-const express = require("express");
-const bodyParser = require("body-parser");
+var nodemailer = require('nodemailer');
 
-const restService = express();
-
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-restService.use(bodyParser.json());
-
-restService.post("/echo", function(req, res) {
- 
- var mailer   = require("mailer")
-  , username = "automataaug2018@gmail.com"
-  , password = "Auto@2018";
-  
-  mailer.send(
-  { host:           "smtp.mandrillapp.com"
-  , port:           587
-  , to:             "autodroid2018@gmail.com"
-  , from:           "automataaug2018@gmail.com"
-  , subject:        "Green Test"
-  , body:           "Hello from eGreen!"
-  , authentication: "login"
-  , username:       username
-  , password:       password
-  }, function(err, result){
-    if(err){
-      console.log(err);
-    }
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
   }
-);
-  
-  return res.json({
-   fulfillmentText: "eGreen Test Success"
-   
-  });
 });
 
-function myData() { 
-   chatResponse=chatResponse+"hello";
-} 
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
 
-restService.listen(process.env.PORT || 587, function() {
-  console.log("Server up and listening");
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
 });
