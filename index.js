e2c4aeb13ad6dbfb93b7673a5d23afd54c4146c3
akshatamr <42598565+akshatamr@users.noreply.github.com>
2018-08-24 10:19:30 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-var nodemailer = require('nodemailer');
+"use strict";
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'automataaug2018@gmail.com',
-    pass: 'Auto@2018'
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res) {
+var chatResponse = "Below is a list of details I could fetch.Spare some time and book wisely , I have used my intelligence and calucalted the carboon footprints for each of the options.";
+var counter = 0;  
+var airlineDetails = require('./airline.json');
+airlineDetails.forEach(function (airlineDetail) {
+  //chatResponse = airlineDetail.departingCity;
+  if(req.body.queryResult.parameters.departCity == airlineDetail.departingCity && req.body.queryResult.parameters.arrivalCity == airlineDetail.arrivalCity)
+  {
+  counter = counter+1;  
+  //var cp = calculateCarbonFootPrint(airlineDetail.stops,airlineDetail.luggage,airlineDetail.runningHours,airlineDetail.fuelEfficiency);
+  var cp = "unclassified";
+    
+   if (airlineDetail.stops < 2  && airlineDetail.luggage < 15 && airlineDetail.runningHours < 12 && airlineDetail.fuelEfficiency > 0.85 )
+   {
+     cp = "low";
+   }
+   
+   if (airlineDetail.stops < 3  && airlineDetail.luggage < 15 && airlineDetail.runningHours < 15 && airlineDetail.fuelEfficiency > 0.75 && airlineDetail.fuelEfficiency < 0.85 )
+   {
+     cp = "average";
+   } 
+       
+   if (airlineDetail.stops > 3 || airlineDetail.luggage > 15 || airlineDetail.runningHours > 15 || airlineDetail.fuelEfficiency < 0.75 )
+   {
+     cp = "very high";
+   }
+    
+  chatResponse = chatResponse + "\n\r\t"+"                                                                                                           "+"Option :" +counter+" "+airlineDetail.name+ " which takes "+airlineDetail.runningHours+" hours and has "+airlineDetail.stops+" stops. The carbon footprint is \n\r\t"+cp+"." ;
   }
+  
+});
+  
+
+   
+  return res.json({
+    fulfillmentText: chatResponse
+});
 });
 
-var mailOptions = {
-  from: 'automataaug2018@gmail.com',
-  to: 'autodroid2018@gmail.com',
-  subject: 'Sending Email for eGreen',
-  text: 'That was easy!'
-};
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
 });
